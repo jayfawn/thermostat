01ce6cc3bc9c952a3fb8f36db61615ce845798ca
Alex Hutchings <alexhutchings4@gmail.com>
2019-02-06 12:04:59 +0000

    temperature able to go up and down

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,4 +7,14 @@ describe('Thermostat', function () {
   it('starts at 20 degrees', function () {
     expect(thermostat.temperature).toEqual(20)
   })
+
+  it('increases in temperature', function () {
+    thermostat.up(2)
+    expect(thermostat.temperature).toEqual(22)
+  })
+
+  it('decreases in temperature', function() {
+    thermostat.down(2)
+    expect(thermostat.temperature).toEqual(18)
+  })
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temperature = 20
 }
+
+Thermostat.prototype.up = function(number) {
+  this.temperature += number
+}
+
+Thermostat.prototype.down = function(number) {
+  this.temperature -= number
+}
