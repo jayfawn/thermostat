989275992ab4bec85d53cfaf7e0ee933d0f8c6a9
Alex Hutchings <alexhutchings4@gmail.com>
2019-02-06 12:33:58 +0000

    Add power saving property, with function to switch it. Add max temperature for power saving on/off

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,9 +1,8 @@
 describe('Thermostat', function () {
   beforeEach(function () {
     thermostat = new Thermostat();
-  }
+  })
 
-  )
   it('starts at 20 degrees', function () {
     expect(thermostat.temperature).toEqual(20)
   })
@@ -16,5 +15,25 @@ describe('Thermostat', function () {
   it('decreases in temperature', function() {
     thermostat.down(2)
     expect(thermostat.temperature).toEqual(18)
+  })
+
+  it('cannot go lower than 10 degrees', function () {
+    thermostat.down(15)
+    expect(thermostat.temperature).toEqual(10)
+  })
+
+  it('has a power saving mode', function () {
+    expect(thermostat.powerSaving).toEqual(true)
+  })
+
+  it('when power saving is on, cannot go above 25 degrees', function () {
+    thermostat.up(7)
+    expect(thermostat.temperature).toEqual(25)
+  })
+
+  it('when power saving is off, cannot go above 32 degrees', function () {
+    thermostat.switchPowerSaving()
+    thermostat.up(15)
+    expect(thermostat.temperature).toEqual(32)
   })
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,28 @@
 function Thermostat() {
   this.temperature = 20
+  this.powerSaving = true
+}
+
+Thermostat.prototype._maxTemp = function () {
+  return (this.powerSaving ? 25 : 32)
 }
 
 Thermostat.prototype.up = function(number) {
-  this.temperature += number
+  if (this.temperature + number > this._maxTemp()) {
+    this.temperature = this._maxTemp()
+  } else {
+    this.temperature += number
+  }
 }
 
 Thermostat.prototype.down = function(number) {
-  this.temperature -= number
+  if (this.temperature - number < 10) {
+    this.temperature = 10
+  } else {
+    this.temperature -= number
+  }
 }
+
+Thermostat.prototype.switchPowerSaving = function () {
+  this.powerSaving ^= true
+};
