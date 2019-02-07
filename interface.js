3a2dfcb0f1dbfdd5c86586478bad0c783448e010
John Haythorn <johnhaythorn@yahoo.co.uk>
2019-02-07 12:32:26 +0000

    html wizardry

--- interface.js
+++ interface.js
@@ -2,11 +2,13 @@ $( document ).ready(function() {
   let thermostat = new Thermostat();
 
   function refreshData() {
-    $('#temperature').text(thermostat.temperature);
+    $('#temperature').text(thermostat.temperature + '\xB0' + 'C');
     $('#energy-usage').text(thermostat.energyUsage());
     $('#energy-usage').attr('class', thermostat.energyUsage());
     $('#temperature').attr('class', thermostat.energyUsage());
 
+    $('#powersaving-on').attr('class', thermostat.powerSavingMode);
+    $('#powersaving-off').attr('class', thermostat.powerSavingMode);
   }
 
   refreshData();
